a284b632427cccb8926b50c401a925f0f800a424
Volodsher <volodsher@gmail.com>
2019-09-17 09:14:09 +0300

    Updates

---
+++ precache-manifest.f08df1b0bbd2427173489a221d0c2de9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cb14490ee27a588d8feaa6c98164ea",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "3418eba3828b77da1a55",
+    "url": "/react_tabs/static/css/main.2b2724e1.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "3418eba3828b77da1a55",
+    "url": "/react_tabs/static/js/main.88991bd4.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
